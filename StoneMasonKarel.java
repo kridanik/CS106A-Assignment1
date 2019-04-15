1e4d39d3c9abf201ba070c8af76bbcff8aeb3e36
kridanik <bojntomic@gmail.com>
2019-04-15 11:17:36 +0200

    Finished StoneMasonKarel.java

--- StoneMasonKarel.java
+++ StoneMasonKarel.java
@@ -1,17 +1,77 @@
 /*
  * File: StoneMasonKarel.java
  * --------------------------
- * The StoneMasonKarel subclass as it appears here does nothing.
- * When you finish writing it, it should solve the "repair the quad"
- * problem from Assignment 1.  In addition to editing the program,
- * you should be sure to edit this comment so that it no longer
- * indicates that the program does nothing.
+ * Karel repairs each column that has stones missing. It starts at 1,1 facing
+ * east. Columns are always four units apart, with first column being at 1,1.
+ * The last column is followed by a wall.
  */
 
 import stanford.karel.*;
 
 public class StoneMasonKarel extends SuperKarel {
 
-	// You fill in this part
-
+	/* Fixes one column at a time, until it hits the wall. */
+	public void run() {
+		while(frontIsClear()) {
+			fixColumn();
+			findColumn();
+		}
+		fixColumn();
+	}
+	
+	/*
+	 * Fixes one column.
+	 * 
+	 * Pre-condition: Facing east at the bottom of a column.
+	 * Post-condition: Facing east at the bottom of the same column.
+	 */
+	private void fixColumn() {
+		turnLeft();
+		repairDamage();
+		descendColumn();
+	}
+	
+	/*
+	 * Ascends and repairs each damaged spot of a column.
+	 * 
+	 * Pre-condition: Facing north at the bottom of a column.
+	 * Post-condition: Facing north at the top of the same column.
+	 */
+	private void repairDamage() {
+		while (frontIsClear()) {
+			if (noBeepersPresent()) {
+				putBeeper();
+			}
+			move();
+		}
+		if (noBeepersPresent()) {
+			putBeeper();
+		}
+	}
+	
+	/*
+	 * Descends a column.
+	 * 
+	 * Pre-condition: Facing north at the top of a column.
+	 * Post-condition: Facing east at the bottom of the same column.
+	 */
+	private void descendColumn() {
+		turnAround();
+		while (frontIsClear()) {
+			move();
+		}
+		turnLeft();
+	}
+	
+	/*
+	 * Moves to the next column.
+	 * 
+	 * Pre-condition: Facing east at the bottom of a column.
+	 * Post-condition: Facing east at the bottom of the next column.
+	 */
+	private void findColumn() {
+		for (int i = 0; i < 4; i++) {
+			move();
+		}
+	}
 }
